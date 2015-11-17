e33de8b01f36142c8e6be29396cc91b875f5741e
Thom <me@thomrichardson.net>
2015-11-16 22:34:18 -0600

    Excluded protractor tests for now

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('pre-test', function () {
 });
 
 gulp.task('test', ['pre-test'], function () {
-    return gulp.src(['test/**/*.js'])
+    return gulp.src(['test/server/**/*.js'])
         .pipe(mocha())
         // Creating the reports after tests ran
         .pipe(istanbul.writeReports())
